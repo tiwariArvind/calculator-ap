7ca839a19d44bd681c6bc75b29aa7bc5dcac1bd0
arvind.tiwari@izea.com <arvind.tiwari@izea.com>
2025-10-22 10:35:18 +0530

    Add new describe block for empty cal

--- spec/string_cal_spec.rb
+++ spec/string_cal_spec.rb
@@ -1,3 +1,10 @@
 require './string_cal.rb'
 RSpec.describe StringCalculator do
+  describe ".add" do
+    context "given an empty string" do
+      it "returns zero" do
+        expect(StringCalculator.add("")).to eql(0)
+      end
+    end
+  end
 end
